eb667cdd19a476d5a0754a045b3c86fc3aa98ad1
tran-tuong <tranvantuong2k3@gmail.com>
2024-06-23 14:41:54 +0700

    Connect to the db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Sever is running on port 3000!");
+  console.log("Sever is running on port 3000");
 });
