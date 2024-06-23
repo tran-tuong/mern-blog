76ea5607c58920faa2a9015cc9ae7b5383d8a06d
tran-tuong <tranvantuong2k3@gmail.com>
2024-06-23 17:39:43 +0700

    add a middleware and a function to handle the errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../ultis/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,7 +13,8 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All field required" });
+    next(errorHandler(400, "All field required"));
+    // return res.status(400).json({ message: "All field required" });
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +29,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("Signup sucessful");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
