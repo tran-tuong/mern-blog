9cd859f7196a557feb9c22c6e82c66145aac2e56
tran-tuong <tranvantuong2k3@gmail.com>
2024-06-25 10:57:43 +0700

    add private Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,11 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import Dashboard from "./pages/About.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import Project from "./pages/Project.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
   return (
@@ -18,7 +19,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/project" element={<Project />} />
       </Routes>
       <Footer />
