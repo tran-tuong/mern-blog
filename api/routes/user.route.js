193dbdc9baac73c7d6c623becbf7e71f117e6c63
tran-tuong <tranvantuong2k3@gmail.com>
2024-06-28 13:21:42 +0700

    add get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
-import { updateUser, deleteUser, signout } from "../controllers/user.controller.js";
+import { getUsers, test } from "../controllers/user.controller.js";
+import {
+  updateUser,
+  deleteUser,
+  signout,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../ultis/verifyUser.js";
 
 const router = express.Router();
@@ -8,6 +12,7 @@ const router = express.Router();
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post("/signout", signout)
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
